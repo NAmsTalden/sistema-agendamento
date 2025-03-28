a9b04da7467acf6a30a5d4902c1e00af7c314edc
NAmsTalden <crispimramosn@gmail.com>
2025-03-27 21:36:09 -0300

    Implementa calendário funcional com Supabase

---
+++ public/js/config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://frvichbilixzpruddyfp.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZydmljaGJpbGl4enBydWRkeWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMzc2MDMsImV4cCI6MjA1ODYxMzYwM30.Ax-mMn62jSTBixYbrP0WAEr3FexxgVAcKEU0S3GSx-0';
+
+// Inicializar cliente Supabase
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY); 
